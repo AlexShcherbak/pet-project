17507e330f61eecf6b1211a3ecb182cb942cbc63
AlexShcherbak <alexshcherbak.dev@gmail.com>
2021-04-17 00:55:10 +0300

    stop on FirebaseState, need to recheck context

---
+++ my-app-list/src/pages/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const About = () => (
+    <div className='jumbotron'>
+        <div className='container'>
+            <h1 className='display-4'>Best React app</h1>
+            <p className='lead'>
+                Version <strong>2.93.2</strong>
+            </p>
+        </div>
+    </div>
+)
