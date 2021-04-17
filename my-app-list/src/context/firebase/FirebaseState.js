897d6a170b68666c6a3c198d7c159b46fba3b852
AlexShcherbak <alexshcherbak.dev@gmail.com>
2021-04-18 00:20:46 +0300

    return 1 notes 2 times in list, need to be recheck

--- my-app-list/src/context/firebase/FirebaseState.js
+++ my-app-list/src/context/firebase/FirebaseState.js
@@ -2,7 +2,7 @@ import React, {useReducer} from 'react';
 import axios from 'axios';
 import {FirebaseContext} from "./firebaseContext";
 import {firebaseReducer} from "./firebaseReducer";
-import {REMOVE_NOTE, SHOW_LOADER} from "../types";
+import {ADD_NOTE, REMOVE_NOTE, SHOW_LOADER, FETCH_NOTES} from "../types";
 
 const url = process.env.REACT_APP_DB_URL;
 
@@ -19,7 +19,17 @@ export const FirebaseState = ({children}) => {
         showLoader();
         const res = await axios.get(`${url}/notes.json`);
 
-        console.log('fetchNotes', res.data);
+        const payload = Object.keys(res.data).map(key => {
+            return {
+                ...res.data[key],
+                id: key
+            }
+        })
+
+        dispatch({
+            type: FETCH_NOTES, payload
+        })
+
     }
 
     const addNote = async title => {
@@ -29,7 +39,15 @@ export const FirebaseState = ({children}) => {
 
         try {
             const res = await axios.post(`${url}/notes.json`, note)
-            console.log('addNote', res.data);
+            const payload = {
+                ...note,
+                id: res.data.name
+            }
+
+            dispatch({
+                type: ADD_NOTE,
+                payload
+            })
         } catch (e) {
             throw new Error(e.message)
         }
